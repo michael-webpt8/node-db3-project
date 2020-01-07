edb7b888462fe50ae1e8b6cdf32bf84168a21471
Michael Williams <manicdesigner@gmail.com>
2020-01-07 15:36:22 -0500

    add first 2 probolem answers for queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,20 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName as 'Product Name',
+       c.CategoryName as 'Category Name'
+FROM `Product` as p
+join `Category` as c
+on p.CategoryId = c.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.Id as 'order id',
+	   s.CompanyName as 'Shipper Company Name'
+	   FROM `Order` as o
+	   join `Shipper` as s
+	   on o.ShipVia = s.Id
+	   WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
