adde711c04fad4c54d4dc01f02d3314d462dd9c2
Michael Williams <manicdesigner@gmail.com>
2020-01-07 16:06:49 -0500

    query 3 completed

--- queries.sql
+++ queries.sql
@@ -19,4 +19,13 @@ SELECT o.Id as 'order id',
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName as 'Product name',
+	   od.Quantity as 'product quantity'
+	   FROM `OrderDetail` as od
+	   join `Product` as p
+	   on od.ProductId = p.Id
+	   join `Order` as o
+	   on od.OrderId = o.Id
+	   WHERE o.Id = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
