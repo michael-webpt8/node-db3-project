d915dec2f275df07b60171d320ce1ae5fcbf841c
Michael Williams <manicdesigner@gmail.com>
2020-01-07 20:52:52 -0500

    findSteps, add and update  completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,9 @@ const db = require('../data/db-config');
 module.exports = {
   find,
   findById,
+  findSteps,
+  add,
+  update,
 };
 
 function find() {
@@ -14,4 +17,23 @@ function findById(id) {
     .where({ id })
     .select()
     .first();
+}
+
+function findSteps(scheme_id) {
+  return db('schemes as s')
+    .join('steps as st', 's.id', 'st.scheme_id')
+    .where({ scheme_id })
+    .select('s.id', 's.scheme_name', 'st.step_number', 'st.instructions');
+}
+
+async function add(scheme) {
+  const [id] = await db('schemes').insert(scheme);
+  return findById(id);
+}
+
+async function update(changes, id) {
+  await db('schemes')
+    .where({ id })
+    .update(changes);
+  return findById(id);
 }
